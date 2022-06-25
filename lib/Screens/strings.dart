0c467d4ac208f57d8ccabdec443b5627b1154991
David Manetta <davidmanetta98@gmail.com>
2022-06-25 12:06:58 +0200

    User Data from oAuth

--- lib/Screens/strings.dart
+++ lib/Screens/strings.dart
@@ -1,13 +1,13 @@
 class Strings {
   // Fitbit Client ID (replace XXX with your client ID)
-  static const String fitbitClientID = 'XXX';
+  static const String fitbitClientID = '238MD9';
 
   // Fitbit Client Secret (replace XXX with your client secret)
-  static const String fitbitClientSecret = 'XXX';
+  static const String fitbitClientSecret = 'bede6803d3a16596b06d2ccd5c57c3ec';
 
   /// Auth Uri (replace XXX with your Uri)
-  static const String fitbitRedirectUri = 'XXX';
+  static const String fitbitRedirectUri = 'fitpet://fitbit/auth';
 
   /// Callback scheme (replace XXX with your callback scheme)
-  static const String fitbitCallbackScheme = 'XXX';
+  static const String fitbitCallbackScheme = 'fitpet';
 }//Strings
